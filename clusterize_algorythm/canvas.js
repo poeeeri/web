869a2336ac7fbbf4b3968ca0323578ecee23e3d4
poeeeri <g2007gle@mail.ru>
2025-04-25 22:38:41 +0700

    modified:   ant_algorythm/ant.js 	modified:   clusterize_algorythm/canvas.js 	modified:   clusterize_algorythm/clastering.html 	modified:   clusterize_algorythm/clust_style.css 	modified:   clusterize_algorythm/range.js 	modified:   decision_tree_algorithm/decision_tree.css 	modified:   decision_tree_algorithm/decision_tree.html 	modified:   decision_tree_algorithm/decision_tree.js 	modified:   genetic_algorythm/gen.css 	modified:   genetic_algorythm/gen.html 	modified:   genetic_algorythm/gen.js 	modified:   index.html 	new file:   neural_network/nav.js 	modified:   neural_network/neural.css 	modified:   neural_network/neural.html

--- clusterize_algorythm/canvas.js
+++ clusterize_algorythm/canvas.js
@@ -55,3 +55,29 @@ document.getElementById("clear_canvas").addEventListener("click", () => {
     updateCanvas();
 });
 
+
+const menuToggle = document.querySelector('.menu_toggle');
+const menu = document.querySelector('.menu');
+
+document.querySelector('.menu_toggle').addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active');
+});
+
+const navLinks = document.querySelectorAll('.link');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        if (window.innerWidth <= 768) {
+            menuToggle.classList.remove('active');
+            menu.classList.remove('active');
+        }
+    });
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 768) {
+        menuToggle.classList.remove('active');
+        menu.classList.remove('active');
+    }
+});
+
